ec8c91b73015724b05efc95d3efa41d652d4a1e9
Marcello Luigi Graziano <marcello@iMac-di-Marcello.local>
2020-01-15 17:42:38 +0100

    Lista reservation in view

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Auth;
+use App\User;
+use App\Reservation;
+
+class ReservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $id = Auth::user()->id;
+        DB::table('reservations')->insert([
+            ['user_id' => "$id", 'state' => 'booked']
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        /*
+        $id = Auth::user()->id;
+        // trova prenotazioni passando id utenti
+        $reservationsUser = Reservation::find($id);
+        */
+
+        // crea elenco prenotazioni da inviare
+        $reservations = Reservation::all();
+
+        return view('welcome', compact('reservations'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
